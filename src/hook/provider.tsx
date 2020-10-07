0a7f58603ea3f59341cd543061f1fbaffdb69004
Alireza Rezania <alireza.rzna@gmail.com>
2020-10-07 11:12:51 +0330

    Revert "pass ToastProvider props to internal Toast component"
    This reverts commit c0f72dcf04499916e302759972dc9ccf16b21d4d.

--- src/hook/provider.tsx
+++ src/hook/provider.tsx
@@ -1,9 +1,8 @@
 import React, { FC, useEffect, useRef, useState } from "react";
 import ToastContext from "./context";
 import Toast from "../toast-container";
-import { Props } from "../toast-container";
 
-const ToastProvider: FC<Props> = ({ children, ...props }) => {
+const ToastProvider: FC = ({ children }) => {
   const toastRef = useRef(null);
   const [refState, setRefState] = useState(null);
 
@@ -14,7 +13,7 @@ const ToastProvider: FC<Props> = ({ children, ...props }) => {
   return (
     <ToastContext.Provider value={refState}>
       {children}
-      <Toast ref={toastRef} {...props} />
+      <Toast ref={toastRef} />
     </ToastContext.Provider>
   );
 };
--- src/toast-container.tsx
+++ src/toast-container.tsx
@@ -4,7 +4,7 @@ import Toast, { ToastOptions, ToastProps } from "./toast";
 
 const dims = Dimensions.get("window");
 
-export interface Props extends ToastOptions {
+interface Props extends ToastOptions {
   offset?: number;
   placement: "top" | "bottom";
 }
