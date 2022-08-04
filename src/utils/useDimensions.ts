570db99fc73256cf50a34721f6d7dc998033e8e7
Mohan Dere <Mohan.Dere@bp.com>
2022-08-04 21:28:41 +0530

    fix(TypeScript): Remove unused '@ts-expect-error' directive

--- src/utils/useDimensions.ts
+++ src/utils/useDimensions.ts
@@ -12,9 +12,7 @@ export function useDimensions() {
     const subscription = Dimensions.addEventListener("change", onChange);
 
     return () => {
-      // @ts-expect-error - React Native >= 0.65
       if (typeof subscription?.remove === "function") {
-        // @ts-expect-error
         subscription.remove();
       } else {
         // React Native < 0.65
