f92cf20d9763da5dd05a334b37de7cd032087e03
sw0ok <p.bialon244@gmail.com>
2023-03-28 08:23:06 +0200

    Test

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { default } from "./toast-container";
 export { default as ToastProvider } from "./hook/provider";
 export { default as useToast } from "./hook/useToast";
 export type {ToastType} from './hook/context'
+//test
